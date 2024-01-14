4cb21576c7bc6f39d035e06f7a67f0271ceab1a8
Rohit Jaryal <rohitjaryal@Ryans-MP.local>
2024-01-14 22:20:35 +0530

    feat: working example of NestJS + Type ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test',
+      database: 'test',
+      synchronize: true, // 'true' Not for production. Results in data loss
+      logging: false,
+      entities: [User],
+      migrations: [],
+      subscribers: [],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
